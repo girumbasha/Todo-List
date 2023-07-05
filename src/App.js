4a593a00a0fcee79d5d51eda01c70170a3e5776a
Girum <girumbasha0@gmail.com>
2023-07-05 08:27:24 -0900

    responsive navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import React from 'react'
-import Navbar from './components/Heading/Navbar'
-import { Route,Router,Routes } from 'react-router-dom'
-import About from './components/Heading/About'
-import Blog from './components/Heading/Blog'
-import Shop from './components/Heading/Shop'
-import Contact from './components/Heading/Contact'
-import Home from './components/Body/Home'
-import Page from './components/Heading/Page'
+import Navbar from './Navbar'
 
 function App() {
   return (
-    <div className='App'>
+    <div>
       <Navbar/>
-  <Routes>
-   <Route path='/'element ={<Home/>}/>
-   <Route path= 'about'element={<About/>}/>
-   <Route path='page'element={<Page/>}/>
-   <Route path ='shop'element={<Shop/>}/>
-   <Route path='contact'element={<Contact/>}/>
-    <Route path='blog'element={<Blog/>}/>
-
-  </Routes>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -3,17 +3,13 @@
 // import App from './App';
 
 // ReactDom.render(<App/>,document.getElementById('root'));
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
     <App />
-    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
